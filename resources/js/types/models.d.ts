58020080f190675c9f01b0564367612dbe3bb4db
Narayan <wovosoft@gmail.com>
2023-07-02 22:45:46 +0600

    updated

--- resources/js/types/models.d.ts
+++ resources/js/types/models.d.ts
@@ -1,12 +1,12 @@
 export namespace App.Models{
 	export interface Account {
-		id: number;
-		user_id: number;
-		account_id: string;
-		email: string;
+		id: number ;
+		user_id: number ;
+		account_id: string | null;
+		email: string ;
 		is_valid: "active" | "inactive";
-		created_at: string;
-		updated_at: string;
+		created_at: string | null;
+		updated_at: string | null;
 		domains: Domain[] | null;
 		certificates: Certificate[] | null;
 		test1: string | number | null;
@@ -15,58 +15,59 @@ export namespace App.Models{
 	}
 
 	export interface Certificate {
-		id: number;
-		domain_id: number;
-		issue_date: string;
-		expiry_date: string;
-		private_key: string;
-		certificate: string;
-		created_at: string;
-		updated_at: string;
+		id: number ;
+		domain_id: number ;
+		issue_date: string ;
+		expiry_date: string ;
+		private_key: string ;
+		certificate: string ;
+		created_at: string | null;
+		updated_at: string | null;
 		account: Account | null;
 		domain: Domain | null;
 		is_expired: boolean;
 	}
 
 	export interface Domain {
-		id: number;
-		account_id: number;
-		domain: string;
-		is_ownership_verified: boolean;
-		created_at: string;
-		updated_at: string;
+		id: number ;
+		account_id: number ;
+		domain: string ;
+		is_ownership_verified: boolean ;
+		created_at: string | null;
+		updated_at: string | null;
 		account: Account | null;
 		certificates: Certificate[] | null;
 		orders: Order[] | null;
 	}
 
 	export interface Order {
-		id: number;
-		domain_id: number;
-		order_id: string;
-		expires: string;
-		created_at: string;
-		updated_at: string;
+		id: number ;
+		domain_id: number ;
+		order_id: string | null;
+		expires: string | null;
+		created_at: string | null;
+		updated_at: string | null;
 		domain: Domain | null;
 		account: Account | null;
 	}
 
 	export interface User {
-		id: number;
-		name: string;
-		email: string;
-		email_verified_at: string;
-		password: string;
-		two_factor_secret: string;
-		two_factor_recovery_codes: string;
-		two_factor_confirmed_at: string;
-		remember_token: string;
-		current_team_id: number;
-		profile_photo_path: string;
-		created_at: string;
-		updated_at: string;
+		id: number ;
+		name: string ;
+		email: string ;
+		email_verified_at: string | null;
+		password: string ;
+		two_factor_secret: string | null;
+		two_factor_recovery_codes: string | null;
+		two_factor_confirmed_at: string | null;
+		remember_token: string | null;
+		current_team_id: number | null;
+		profile_photo_path: string | null;
+		created_at: string | null;
+		updated_at: string | null;
 		accounts: Account[] | null;
 		domains: Domain[] | null;
 		profile_photo_url: unknown;
 	}
+
 }
